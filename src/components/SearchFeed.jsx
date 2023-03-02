c1daea5ea58e4a35c1374567542b79f57564365c
alinpeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2023-03-02 11:20:21 +0200

    Update SearchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -16,16 +16,6 @@ const SearchFeed = () => {
 
   return (
     <Box sx={{ overflow: "auto", height: "90vh", flex: 2, marginLeft: "25px" }}>
-      <Typography
-        variant={{ xs: "h5", md: "h4" }}
-        fontWeight="bold"
-        mb={5}
-        sx={{ color: "white" }}
-      >
-        Search results for{" "}
-        <span style={{ color: "#f31503" }}>{searchTerm}</span>
-      </Typography>
-
       <Videos videos={videos} />
     </Box>
   );
