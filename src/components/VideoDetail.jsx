fa0935f04a14fbaf10ffd9d54ddc248532f6c5f6
alinpeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2023-03-02 11:25:07 +0200

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -9,7 +9,7 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 console.log(isMobile)
-const width = (isMobile ? 0.9 * window.innerWitdh : 0.65 * window.innerWitdh);
+const width = (isMobile ? 0.9 : 0.65);
 console.log(width, window.innerWidth)
 
 const VideoDetail = () => {
