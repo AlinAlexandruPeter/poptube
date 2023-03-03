8ee995cc2a4af655680e808270c03e25b4218df5
alinpeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2023-03-03 16:13:49 +0200

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,23 +1,30 @@
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import YouTube from "react-youtube";
 import { Typography, Box, Stack } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
 import { Videos } from "./";
+import IOSSwitch from "./IOSSwitch";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-console.log(isMobile)
-let width = (isMobile ? 0.9 : 0.65);
-width *= window.innerWidth;
-console.log(width, window.innerWidth)
+const percentage = isMobile ? 0.9 : 0.65;
 
 const VideoDetail = () => {
+  const [autoplay, setAutoplay] = useState(false);
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const onVideoEnd = () => {
+    if (autoplay) navigate(`/video/${nextId}`);
+  };
 
   const [videoDetail, setVideoDetail] = useState(null);
   const [videos, setVideos] = useState(null);
+
+  let nextId;
+  if (videos?.length) nextId = [videos[0].id.videoId];
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
@@ -47,9 +54,10 @@ const VideoDetail = () => {
                 marginInline: "5%",
               }}
               opts={{
-                width: width,
-                height: width * 9 / 16,
+                width: percentage * window.innerWidth,
+                height: percentage * window.innerHeight,
               }}
+              onEnd={onVideoEnd}
             />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
@@ -90,7 +98,36 @@ const VideoDetail = () => {
           py={{ md: 1, xs: 5 }}
           justifyContent="center"
           alignItems="center"
+          sx={{ position: "relative" }}
         >
+          <Stack
+            direction="row"
+            sx={{
+              borderBottom: "1px solid #ccc",
+              background: "black",
+              position: "absolute",
+              width: "100%",
+              top: "0",
+              right: "0",
+              zIndex: "5",
+            }}
+            mb={2}
+            p={1}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Typography
+              variant={{ xs: "h5", md: "h4" }}
+              color="#fff"
+              fontSize={25}
+            >
+              <label htmlFor="autoplay">Autoplay</label>
+            </Typography>
+            <IOSSwitch
+              name="autoplay"
+              onClick={() => setAutoplay((prevStatus) => !prevStatus)}
+            />
+          </Stack>
           <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
